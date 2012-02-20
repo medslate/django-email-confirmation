163b153c36394d9c96aeabadecd7244fd97a9b07
Filip Wasilewski <en@ig.ma>
2012-02-20 21:40:52 +0100

    Making sure that only proper user can confirm email address.

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -7,8 +7,19 @@ from emailconfirmation.models import EmailConfirmation
 def confirm_email(request, confirmation_key):
     
     confirmation_key = confirmation_key.lower()
-    email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
     
+    confirmation = None
+    try:
+        confirmation = EmailConfirmation.objects.get(
+            confirmation_key=confirmation_key)
+    except EmailConfirmation.DoesNotExist:
+        pass
+
+    if confirmation and confirmation.email_address.user == request.user:
+        email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
+    else:
+        email_address = None
+
     return render_to_response("emailconfirmation/confirm_email.html", {
         "email_address": email_address,
     }, context_instance=RequestContext(request))
